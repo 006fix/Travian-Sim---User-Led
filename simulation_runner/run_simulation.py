5f9e475b9877a98f1b1e9a06ab3f72a96b056017
Fix <pye.a.c.a@gmail.com>
2025-10-21 13:10:02 +0100

    ammended run setup to ensure functionality

--- simulation_runner/run_simulation.py
+++ simulation_runner/run_simulation.py
@@ -1,7 +1,15 @@
+import sys
+from pathlib import Path
+if __package__ in (None, ""):
+    ROOT = Path(__file__).resolve().parents[1] # repo root
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+#testing line, left in for functionality
+#print(sys.path[0])
+
 from master_controller.simulation_constraints import create_simulation_contraints
 from Specific_Functions.map_creation import map_creation, modify_base_map
 
 map_radius = create_simulation_contraints()
 base_map = map_creation(map_radius)
 base_map = modify_base_map(base_map)
-
