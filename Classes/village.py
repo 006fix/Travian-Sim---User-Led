fe880d6c00830d37412cb93d176e45d9f665c516
Fix <pye.a.c.a@gmail.com>
2025-10-21 15:22:55 +0100

    adding basic version of the village class

---
+++ Classes/village.py
@@ -0,0 +1,25 @@
+import Classes.base_squares as base_squares
+import Base_Data.Building_Data as b_data
+import Base_Data.Fields_Data as f_data
+import Generic_Functions.generic_functions as gen_func
+import random
+
+class Village(base_squares.Square):
+    def __init__(self, location, type_hab, field_list_dict, owner, type_square='village'):
+        super.__init__(location)
+        self.location = location
+        self.interactable = True
+        self.type_hab = type_hab
+        self.fields = field_list_dict
+        self.type_square = type_square
+        self.owner = owner
+        self.buildings = {0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '',
+                    9: '', 10: '', 11: '', 12: '', 13: '', 14: '', 15: '', 16: '',
+                    17: '', 18: '', 19: '', 20: '', 21: '', 22: ''}
+        #structure of the below - reference key for buildings_dict lookup, level, upgradeable bool.
+        self.buildings[0] = ['main_building', 1, True]
+        self.buildings[1] = ['warehouse', 0, True]
+        self.buildings[2] = ['granary', 0, True]
+
+
+        ##LOTS MORE TO COME HERE
--- master_controller/simulation_constraints.py
+++ master_controller/simulation_constraints.py
@@ -1,5 +1,5 @@
 import random
 
-def create_simulation_constraints(rng_seed=42, map_radius = 5):
+def create_simulation_constraints(rng_seed=42, map_radius = 40):
     random.seed(rng_seed)
     return map_radius
