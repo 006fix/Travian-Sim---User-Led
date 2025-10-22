597b050b6439a82b097360c335ce24ec21590175
Fix <pye.a.c.a@gmail.com>
2025-10-22 08:29:30 +0100

    adding ability to create villages for each player

--- Specific_Functions/populate_players.py
+++ Specific_Functions/populate_players.py
@@ -1,4 +1,6 @@
 import Classes.player as player
+import Specific_Functions.village_creation as village_creation
+import random
 
 
 def populate_players(num_players):
@@ -8,4 +10,29 @@ def populate_players(num_players):
         #key for below = name, quadrant, race
         hold_player = player.Player(name, None, None)
         player_dict[name] = hold_player
+    return player_dict
+
+
+def populate_players_with_villages(map_dict, num_players, rng_holder=None):
+    if rng_holder is None:
+        rng_holder = random
+    player_dict = populate_players(num_players)
+    quadrant_options = [("+", "+"), ("+", "-"), ("-", "+"), ("-", "-")]
+    failed_players = []
+    for key in player_dict:
+        active_player = player_dict[key]
+        if active_player.quadrant is None:
+            active_player.quadrant = rng_holder.choice(quadrant_options)
+        try:
+            village_key = village_creation.create_village(
+                map_dict,
+                active_player.name,
+                active_player.quadrant,
+                rng_holder,
+            )
+            active_player.villages.append(village_key)
+        except RuntimeError:
+            failed_players.append(active_player.name)
+    if len(failed_players) > 0:
+        print("Players unable to receive villages:", failed_players)
     return player_dict
